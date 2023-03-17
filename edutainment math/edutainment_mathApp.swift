5fe5fd2d2ba7c1924082fdf81f5171fee53cd5b8
SeikaHirori <SeikaHirori@outlook.com>
2023-03-17 12:33:05 -0700

    Initial Commit

---
+++ edutainment math/edutainment_mathApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainment_mathApp.swift
+//  edutainment math
+//
+//  Created by Seika Hirori on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct edutainment_mathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ edutainment mathUITests/edutainment_mathUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  edutainment_mathUITestsLaunchTests.swift
+//  edutainment mathUITests
+//
+//  Created by Seika Hirori on 3/17/23.
+//
+
+import XCTest
+
+final class edutainment_mathUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
